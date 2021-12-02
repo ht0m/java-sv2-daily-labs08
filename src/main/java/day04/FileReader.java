e7f6b85c589e2979e3dae8f84adc0e9378c2c229
ht0m <hartht@gmail.com>
2021-12-02 16:10:12 +0100

    Filereader

---
+++ src/main/java/day04/FileReader.java
@@ -0,0 +1,32 @@
+package day04;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileReader {
+
+    public int findsSmallestTemperatureSpread(String path) {
+        List<String> file = new ArrayList<>();
+
+        try {
+            file = Files.readAllLines(Paths.get(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int minSpread = 1000;
+        int day = 0;
+
+        for (int i = 2; i < 32; i++) {
+            int dailyMax = Integer.parseInt(file.get(i).substring(6, 8).trim());
+            int dailyMin = Integer.parseInt(file.get(i).substring(12, 14).trim());
+            if (dailyMax - dailyMin < minSpread) {
+                minSpread = dailyMax - dailyMin;
+                day = Integer.parseInt(file.get(i).substring(2, 4).trim());
+            }
+        }
+        return day;
+    }
+}
